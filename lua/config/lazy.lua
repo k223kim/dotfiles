046095c3a04333f852d4fa81c9fda4bd82c40229
Kaeun Kim <k223kim@uwaterloo.ca>
2024-08-05 19:28:27 +0900

    feat: added some keymaps and autocmds

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,13 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lsp.neoconf" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
